83ad96a0f308afddbe4e4d6816e736e6a5c84127
Jiahuan He <jiahuan_he@outlook.com>
2017-10-02 15:59:13 -0600

    Implementing /create, encountered concurrency problem. TODO: add thread-safe map

--- a1-go-solution/server.go
+++ a1-go-solution/server.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	//"time"
+	//"sync"
 )
 
 type ChatRoom struct{
@@ -14,9 +16,13 @@ type ChatRoom struct{
 	history []string
 }
 
+
+
 func main() {
 	ln, _ := net.Listen("tcp", ":8088")
 	allRooms := map[string]ChatRoom{"default-room":ChatRoom{name:"default-room"}}
+	//allRoomsMap := sync.Map{}
+
 	defaultRoom := allRooms["default-room"]
 
 	for {
@@ -27,7 +33,7 @@ func main() {
 		newClient.conn = &newConn
 		newClient.currentRoom.connectedSockets = append(newClient.currentRoom.connectedSockets, &newClient)
 		fmt.Println("log: current user number: "+ strconv.Itoa(len(defaultRoom.connectedSockets)))
-		go newClient.newListener()
+		go newClient.newListener(allRooms)
 	}
 }
 
@@ -38,21 +44,48 @@ type ServerClient struct {
 	currentRoom *ChatRoom
 }
 
-func (client *ServerClient) newListener()  {
+func (client *ServerClient) newListener(allRooms map[string]ChatRoom)  {
 	//Prompt user to enter their name
 	client.send("SYSTEM => Please enter your name: ")
 	name, _ := bufio.NewReader(*client.conn).ReadString('\n')
 	client.name = strings.TrimSuffix(name, "\n")
 	client.currentRoom.broadcast("Welcome "+client.name+" joining " + client.currentRoom.name, nil)
+
 	for {
 		message, err := bufio.NewReader(*client.conn).ReadString('\n')
+		message = strings.TrimRight(strings.TrimSpace(message), "\n")
+		command := strings.Fields(message)
+		fmt.Print("Message Received:", string(message))
 		if err != nil{
 			break
 		}
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		client.currentRoom.broadcast("("+client.currentRoom.name + ") " + client.name + " => "+ message, client)
-		client.send("("+client.currentRoom.name + ") me" + " => "+ message)
+		switch command[0] {
+		case "/create":
+			//c:= make(chan ChatRoom)
+			for _,roomName := range command[1:] {
+				//TODO solve concurrency problem!!
+				if _, ok:=allRooms[roomName]; ok{
+					client.send("SYSTEM => Error: "+roomName+" already exists")
+					break
+				}
+				fmt.Println(roomName)
+
+				allRooms[roomName] = ChatRoom{name:roomName}
+				// A workaround
+				//time.Sleep(100*time.Millisecond)
+				client.send("SYSTEM => Success: "+roomName+" created")
+			}
+
+		case "/leave":
+
+		case "/join":
+		case "/list":
+		case "/switch":
+		case "/rename":
+		default:
+			client.currentRoom.broadcast("("+client.currentRoom.name + ") " + client.name + " => "+ message, client)
+			client.send("("+client.currentRoom.name + ") me" + " => "+ message)
+		}
 	}
 }
 
@@ -60,7 +93,7 @@ func (clent *ServerClient) send(message string) {
 	(*clent.conn).Write([]byte(message + "\n"))
 }
 
-func (chatRoom ChatRoom) broadcast(message string, selfClient *ServerClient){
+func (chatRoom *ChatRoom) broadcast(message string, selfClient *ServerClient){
 	for _,client := range chatRoom.connectedSockets{
 
 		//fmt.Println(client.name)
@@ -74,7 +107,3 @@ func (chatRoom ChatRoom) broadcast(message string, selfClient *ServerClient){
 		client.send(message)
 	}
 }
-
-
-
-
